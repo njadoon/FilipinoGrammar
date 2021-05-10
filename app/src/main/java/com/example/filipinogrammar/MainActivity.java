4643f63897fa334cc5f4043301c5e293fb488e98
njadoon <jadoon.naif@ue.edu.ph>
2021-05-10 05:22:25 +0300

    Start Screen update

--- app/src/main/java/com/example/filipinogrammar/MainActivity.java
+++ app/src/main/java/com/example/filipinogrammar/MainActivity.java
@@ -1,12 +1,28 @@
 package com.example.filipinogrammar;
 
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.appcompat.widget.AppCompatEditText;
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+        AppCompatEditText etName = findViewById(R.id.etName);
+        Button btnStart = findViewById(R.id.btnStart);
+
+        btnStart.setOnClickListener(v -> {
+            if (etName.getText().toString().isEmpty()){
+                Toast.makeText(getApplicationContext(), "Please enter your name", Toast.LENGTH_SHORT).show();
+            } else {
+                Intent intent = new Intent(this, questions.class);
+                startActivity(intent);
+                finish();
+            }
+        });    }
 }
---
+++ app/src/main/java/com/example/filipinogrammar/questions.java
@@ -0,0 +1,13 @@
+package com.example.filipinogrammar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class questions extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_questions);
+    }
+}

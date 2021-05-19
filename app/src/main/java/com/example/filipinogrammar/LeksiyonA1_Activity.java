9d9fd10fd936666ab4269329baf3de53192dd8ed
Waquil Carim <CARIM.FAISALWAQUIL@UE.EDU.PH>
2021-05-19 08:23:04 +0800

    Added new pages and icon

--- app/src/main/java/com/example/filipinogrammar/LeksiyonA1_Activity.java
+++ app/src/main/java/com/example/filipinogrammar/LeksiyonA1_Activity.java
@@ -6,35 +6,35 @@ import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Choice_Activity extends AppCompatActivity {
-    Button leksiyonbtn, pagsusulitbtn;
+public class LeksiyonA1_Activity extends AppCompatActivity {
+    Button nextbtn, qzskpbtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_choice_);
+        setContentView(R.layout.activity_leksiyon_a1);
 
-        leksiyonbtn = (Button) findViewById(R.id.btnlesson);
-        leksiyonbtn.setOnClickListener(new View.OnClickListener() {
+        nextbtn = (Button) findViewById(R.id.btnNext);
+        nextbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                openLeksiyonActivity();
+                openNextActivity();
             }
         });
 
-        pagsusulitbtn = (Button) findViewById(R.id.btnquiz);
-        pagsusulitbtn.setOnClickListener(new View.OnClickListener() {
+        qzskpbtn = (Button) findViewById(R.id.btnskip);
+        qzskpbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                openQuizActivity();
+                openSkiponQuizActivity();
             }
         });
     }
-    public void openLeksiyonActivity() {
-        Intent intent = new Intent(this, LeksiyonA1_Activity.class);
+    public void openNextActivity() {
+        Intent intent = new Intent(this, LeksiyonA2_Activity.class);
         startActivity(intent);
     }
-    public void openQuizActivity() {
+    public void openSkiponQuizActivity() {
         Intent intent = new Intent(this, Quiz_Activity.class);
         startActivity(intent);
     }

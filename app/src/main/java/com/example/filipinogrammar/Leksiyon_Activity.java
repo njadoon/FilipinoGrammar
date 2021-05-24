e3699f561b1bc5f238a337ea250549e671e13c7f
Waquil Carim <CARIM.FAISALWAQUIL@UE.EDU.PH>
2021-05-24 16:27:10 +0800

    Added new codes

--- app/src/main/java/com/example/filipinogrammar/Leksiyon_Activity.java
+++ app/src/main/java/com/example/filipinogrammar/Leksiyon_Activity.java
@@ -3,22 +3,34 @@ package com.example.filipinogrammar;
 import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class Leksiyon_Activity extends AppCompatActivity {
     Button nextbtn, qzskpbtn;
+    TextView lessonHeader, lesson;
+    int lessons = 0;
+    private Lessons current;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_leksiyon);
+        lessonHeader = findViewById(R.id.lesson1);
+        lesson = findViewById(R.id.text1);
 
         nextbtn = (Button) findViewById(R.id.btnNext);
         nextbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                openNextActivity();
+                lessons++;
+
+                lessonHeader.setText(current.getLessonHeader(lessons));
+                lesson.setText(current.getLesson(lessons));
+                if(lessons == 10){
+                    nextbtn.setVisibility(View.INVISIBLE);
+                }
             }
         });
 
@@ -30,10 +42,7 @@ public class Leksiyon_Activity extends AppCompatActivity {
             }
         });
     }
-    public void openNextActivity() {
-        Intent intent = new Intent(this, Leksiyon_Activity.class);
-        startActivity(intent);
-    }
+
     public void openSkiponQuizActivity() {
         Intent intent = new Intent(this, questions.class);
         startActivity(intent);
---
+++ app/src/main/java/com/example/filipinogrammar/Lessons.java
@@ -0,0 +1,120 @@
+package com.example.filipinogrammar;
+
+public class Lessons {
+
+    public String lessonheader[] = {
+            /*Array1*/"Leksiyon I",
+            /*Array2*/"Leksiyon II",
+            /*Array3*/"Leksiyon III",
+            /*Array4*/"Leksiyon IV",
+            /*Array5*/"Leksiyon V",
+            /*Array6*/"Leksiyon VI",
+            /*Array7*/"Leksiyon VII",
+            /*Array8*/"Leksiyon VIII",
+            /*Array9*/"Leksiyon IX",
+            /*Array10*/"Leksiyon X"
+    };
+
+    public String lessons[] = {
+            /*Array1*/"Ang salitang 'nang' ay isang pagsabay at ang salin nito sa ingles ay when so that\n\n\n\nHalimbawa: \n\na) 'Nagulat ako nang nakita ko sila'\n\nb) 'Kumain ka na, nang (sa ganoon ay) hindi ka na magutom'\n\n\n\nAng salitang nang ay nangangahulugan din ng isang pang-abay o pariralang pang-abay.\n\n\n\nInilalarawan ng pang-abay ang pandiwa, Halimbawa:\n\na) Kumain ka na. - Kumain ka nang marami.\n" +
+                    "\n\nb) Uminom ka. - Uminom ka nang kaunti.\n\nc) Sila ay tumakbo. - Sila ay tumakbo nang mabilis.\n\n" +
+                    "Ang paggamit naman ng salitan 'ng' ay naaayon sa mga sumusunod:\na)Bilang isang marker ng bagay, inilagay sa harap ng pangngalan na parang ito ay isang pang-ukol sa Ingles. \nb)Bilang isang nagmamay-ari na marker para sa mga hindi tiyak na bagay.\n\n" +
+                    "Halimbawa:\na) 'Kumáin siya ng maráming sísig at kánin'\n" +
+                    "b)'Ang kagúbatan ang tahánan ng maráming háyop'\n\nPara sa pangalawang paggamit, ihambing ang ni (at nina) sa ng:\n\na)'Ang kagúbatan ang tahánan ni Roberto, ang malakíng tumanggóng'\nb)'Ang báhay iyán niná Juan at Miguel'\n\n",
+            /*Array2*/"Paggamit ng 'Niyo' sa halip na 'Ninyo' o 'N'yo'\nHalimbawa:\n\nBarkada: Hindi mo man kadugo pero and turingan NIYO sa isa't isa ay pamilya.\nTandaan: Walang Salitang 'Niyo'. Ang tamang salita ay  'Ninyo' o kaya naman ay 'N'yo'. \n\n Paggamit ng 'Sayo' sa halip na 'sa iyo' o 'Sa'yo'\nHalimbawa:\na) 'Para SAYO ang laban na 'to'\nTandaan: Walang salitang 'Sayo'. Ang tamang salita ay 'Sa Iyo' o kaya naman ay 'Sa'yo'."+
+                    "\n\nPaggamit ng 'Niyo' sa halip na 'Ninyo' o 'N'yo'\nHalimbawa:\n\nBarkada: Hindi mo man kadugo pero turingan NIYO sa isa't isa ay pamilya.\nTandaan: Walang salitang 'Niyo'. Ang tamang salita ay 'Ninyo' o Kaya naman ay 'N'yo'.\n\n\nPaggamit ng 'Sayo' sa halip na 'Sa Iyo' o 'Sa'yo'\nHalimbawa:a) Para SAYO and laban na 'to.\nTandaan: Walang salitang SAYO. Ang tamang salita ay 'Sa Iyo' o kaya naman ay Sa'yo.",
+            /*Array3*/"Ang wastong paggamit ng 'May' at 'Mayroon'.\nGinagamit ang 'May' kung ito'y sinusundan ng mga sumusunod na bahagi ng pananalita:\n\nPangalan - MAY prutas siyang dala.\nPandiwa - MAY kumakatok sa labas.\nPang-uri - MAY matalino siyang anak.\nPanghalip na Paari - MAY kanila silang ari-arian.\nPantukoy na Mga - MAY mga lalaking naghihintay sa iyo.\nPang-ukol na Sa - MAY sa-ahas pala ang kaibigan mo."+
+                    "\n\nGinagamit ang 'Mayroon kung ito'y:\nSinusundan ng isang kataga o ingklitik. Halimbawa:\na)MAYROON ba siyang pasalubong\nb)MAYROON nga bang bagong Pajero sila?\n\nSinusundan ng panghalip palagyo. Halimbawa:\n   a)MAYROON siyang kotse.\nb)MAYROON kaming palaisdaan sa Bulacan.\nc)MAYROON tayong pagsusulit sa Filipino bukas.\n\nNangangahulugang 'mayaman'. Halimbawa:\n" +
+                    "a) Ang pamilya ni Carol ay MAYROON sa kanilang lalawigan.\nb)Siya lamang MAYROON sa aming magkakapatid.",
+            /*Array4*/"Ang wastong paggamit ng 'Daw'/'Din' at 'Daw'/'Raw':\nGinagamit and 'Daw'/'Din' kapag ang salitang sinusundan ay nagtatapos ng katinig; at 'Raw'/'Rin' kapag nagtatapos sa patinig.\nHalimbawa:\n a)May sayawan DAW sa plasa.\nb)Sasama RAW siya sa atin.\n\n\nWastong paggamit ng 'Kung' at 'Kong'\nGinagamit and 'Kung' bilang pangatnig na panubali. Katumbas nito and 'if' sa Ingles. Ang 'Kong' ay panghalip panao sa kanukalang paari.\nHalimbawa:\na)Matutulog na ako KUNG papatayin mo na ang ilaw.\nb)Nabasa ang binili KONG aklat."+
+                    "Ang wastong paggamit ng salitang 'Kung Di' at 'Kundi'.\nAng 'Kung Di' ay nanggaling sa salitang 'Kung Hindi' o 'if not' sa Ingles. Ang 'Kundi' naman ay 'except'.\nHalimbawa:\na)AAlis na sana kami KUNG DI ka dumating.\nb) Walang sinuman ang pwedeng manood KUNDI iyong mga may tiket lamang."+
+                    "Ang wastong paggamit ng salitang 'Pinto' at 'Pintuan'.\nAng 'Pinto'(door) ay isang bahagi ng daanan na isinasara at ibinubukas. Samantala, ang 'Pintuan'(doorway) ay ang bahagi ng kinalalagyan ng pinto.\nHalimbawa:\na)May kumakatok, buksan mo nga ang PINTO\nb)Natanggal ang pinto sa PINTUAN.",
+            /*Array5*/"Ang wastong paggamit sa salitang 'Hagdan' at 'Hagdanan'.\nAng 'Hagdan'(stairs) ay ang baytang inaakyatan at binababaan. Samantala, ang 'Hagdan'(stairway) ay ang bahaging kinalalagyan ng hagdan.\nHalimbawa:\na)Nagmamadaling inakyat ni Marvin ang mga HAGDAN.\nb)Ilagay mo ang HAGDANAN sa tapat ng bintana.\n\n\nAng wastong paggamit ng salitang 'Pahirin' at 'Pahiran'; Punasin at Punasan.\nAng pahirin at punasin (wipe off) ay nangangahulugang alisin o tanggalin.\n" +
+                        "Ang pahiran at punasan (to apply) ay nangangahulugang lagyan.\nHalimbawa:\na)Pahirin mo ang mga luha sa iyong mga mata.\n" +
+                        "b) Pahiran mo ng palaman ang tinapay.\n" +
+                        "c) Punasin mo ang pawis sa iyong likod.\n" +
+                        "d) Punasan mo ng alkohol ang iyong mga binti.\n",
+            /*Array6*/"Ang wastong paggamit sa salitang 'Bitiwan' at 'Bitawan'.\nAng wastong gamit ay bitiwan at hindi bitawan. Ang salitang-ugat ay bitiw at \n" +
+                    "hindi bitaw. Ang salitang \"bitaw\" ay ginagamit sa pagsasabong ng manok samantalang ang \"bitiw\" ay \n" +
+                    "sa pagkawala o pag-alis sa pagkakahawak.\nHalimbawa:\na)Bitiwan mo ang mga braso ko kung hindi ay sisigaw ako.\n" +
+            "b)Nabitiwan ni Aling Nena ang mga pinamalengke dahil sa takot.\n" +
+            "c)Binitawan ni Mang Kanor anga kabayong nagwawala.\n" +
+            "d)\"Bitiwan mo muna yang mga hawak mong holen at tulungan mo ako sa pag-iigib\", ang sigaw ni \n" +
+            "e)Boyet sa kapatid.\n" +
+            "f)Bitiwan mo na ang pag-asang babalikan ka pa ng iyong kasintahan.\nHalimbawa:a)Ang \"bitawan\" ay nauukol sa pook na pinagdarausan ng paglalaban ng mga manok na walang tari. \n" +
+            "b)Ito ay maaring sa kalsada o bakuran.\n" +
+            "c)Ang \"bitaw\" ay idinaraos upang bigyan ng pagsasanay ang mga manok.",
+            /*Array7*/"Ang wastong paggamit sa salitang KAPAG at KUNG - Ipinakikilala ng kung ang di-katiyakan ng isang kalagayan. Ipinakikilala ng kapag ang isang kalagayang tiyak.\nHalimbawa:\na)Umuuwi siya sa probinsiya kapag araw ng Sabado.\n" +
+            "b)Hindi niya masabi kung Sabado o Linggo ang pag-uwi niya sa probinsiya.\n" +
+            "c)Mag-ingat ka naman kapag nagmamaneho ka.\n" +
+            "d)Mag-ingat ka kung ikaw ang magmamaneho ng kotse\n\n\nAng wastong paggamit sa salitang KIBO at IMIK - Pagkilos ang tinutukoy ng kibo.  Pangungusap ang tinutukoy ng imik.\nHalimbawa:\na)Wala siyang kakibu-kibo kung matulog.\n" +
+            "b)Hindi siya nakaimik nang tanungin ko.\nTandaan:\n" +
+            "Hindi lamang sa tao nagagamit ang kibo.\nHalimbawa:\n" +
+            "a)Kumikibo nang bahagya ang apoy ng kandila. \n" +
+            "b)Huwag ninyong kibuin ang mga bulaklak na iniayos ko sa plorera.",
+            /*Array8*/"Ang wastong paggamit sa salitang SUNDIN at SUNDAN\n" +
+            "Ang sundin (to obey) ay nangangahulugan ng pagsunod sa payo o pangaral; ang sundan\n" +
+            "(to follow) ay gayahin o pumunta sa pinuntahan ng iba.\nHalimbawa:\na)Sundin mong lagi ang sinasabi ng iyong mga magulang dahil para rin iyon sa iyong kabutihan.\n" +
+            "b)Sundan mo ang mga kabayanihang ipinakita ng iyong ama sa bayan.\n" +
+            "c)Sundan mo siya baka siya maligaw.\n\n\n Ang wastong paggamit sa salitang SUBUKIN at SUBUKAN\n" +
+            "Subukin (to test, to try) – masubok ang husay o galing ng isang bagay o gawain;\n" +
+            "Subukan (to see secretly) – palihim na pagmamatyag o pag-eespiya sa kilos ng isang tao.\nHalimbawa:\na)Susubukin ko muna kung maayos itong kompyuter bago ko bilhin.\n" +
+            "b)Subukan mo nga kung ano ang ginagawa ng mga bata sa likod-bahay.\nAng wastong paggamit sa salitang HATIIN at HATIAN\n" +
+            "Hatiin (to divide) – partihin;\nHatian (to share) – ibahagi\nHalimbawa:\na)Hatiin mo sa anim ang pakwan.\n" +
+            "b)Hinahatian niya ng kanyang hamburger ang namamalimos na bata.",
+            /*Array9*/"Ang wastong paggamit sa salitang  IWAN at IWANAN\n" +
+            "Iwan (to leave something or somebody) – huwag isama;\n" +
+            "Iwanan (to leave something to somebody) – bigyan. \nHalimbawa:\na) Iniwan ni Arnie ang kotse sa garahe.\n" +
+            "b)Iniwanan ni Rene ng pera si Joy bago siya umalis.\n\n\nAng wastong paggamit sa salitang TAGA at TIGA\n" +
+            "Walang unlaping tiga-. Taga- ang dapat gamitin. Ginagamit ang gitling sa unlaping taga-kung sinusundan ng pangngalang pantangi.\nHalimbawa:\na) Si G. Caniete ay taga-Bikol.\n" +
+            "b)Taganayon ang magandang babaeng iyon\n\n\nAng wastong paggamit sa salitangWALISAN at WALISIN\n" +
+            "Walisin ang isang bagay na maaring tangayin ng walis\n" +
+            "Halimbawa:\n" +
+            "a)Walisin mo ang mga tuyong dahon sa bakuran.\n" +
+            "b)Aking wawalisin ang mga alikabok sa aking kuwarto.\n" +
+            "c)Tayo nang walisin ang mga dumi sa sahig.\n" +
+            "d)Nais kong walisin ang nagkalat na papel sa aklatan.\nWalisan ang pook o lugar\nHalimbawa:\n" +
+            "a)Napakarumi ng iyong silid. Pwede bang walisan mo naman 'yan?\n" +
+            "b)Aking wawalisan ang silid-aklatan dahil may bibisita bukas.\n" +
+            "c)Walisan mo ang ating bakuran. Tambak ito ng mga tuyong dahon.\n" +
+            "d)Nais kong walisan ang aklatan.\nTandaan:\n" +
+            "Gamitin ang salitang walisin kung ang ibig tukuyin ay ang pag-aalis ng partikular na dumi o kalat.\n" +
+            "Gamitin ang salitang walisan kung ang tinutukoy ay isang partikular na lugar na marumi.",
+            /*Array10*/"Ang wastong paggamit sa salitang HATIIN at HATIAN\n" +
+            "Hatiin ( to divide) o partihin\n" +
+            "Hatian ( to share) o ibahagi.\n" +
+            "Halimbawa:\n" +
+            "a)Hatiin mo sa amin ang pakwan.\n" +
+            "b)Hinatian niya ng kanyang hamburger ang namamalimos na bata.\n\n\nAng wastong paggamit sa salitang NABASAG at BINASAG\n" +
+            "Ang salitang nabasag ay nangangahulugan ng kilos na di sinasadya o di ginusto\n" +
+            "Ang salitang binasag naman ay nagpapakita ng sariling pagkukusa.\n" +
+            "Halimbawa:\n" +
+            "a)Galit na galit na binasag ng lalaki ang mga salamin ng kotse.\n" +
+            "b)Nagmamadali kasi siyang maghusga kaya nabasag niya ang mga plato.\n\n\nAng wastong paggamit sa salitang KUMUHA at MANGUHAKumuha- to get\n" +
+            "Manguha – to gather, to collect\n" +
+            "Halimbawa:\n" +
+            "a)Kumuha ng isang basong tubig si Neth para kay Jean.\n" +
+            "b)Nanguha ng mga kabibe ang mga bata sa dalampasigan.\n\n\nAng wastong paggamit sa salitang KATA at KITA\n" +
+            "Kata - ikaw at ako\n" +
+            "Kita - ikaw\n" +
+            "Halimbawa:\n" +
+            "a)Manood kata ng sine.\n" +
+            "b)Iniibig kita.\n\n\nAng wastong paggamit sa salitang KILA at KINA\n" +
+            "Walang salitang kila.Ang Kina ay maramihan ng kay.\n" +
+            "Halimbawa:\n" +
+            "a)Pakidala ang mga laruang ito kina Benny at Maris.\n" +
+            "b)Makikipag-usap ako kina Vic at Nona.\n"
+    };
+
+    public String getLessonHeader(int a){
+        String header = lessonheader[a];
+        return header;
+    }
+
+
+    public String getLesson(int b){
+        String lesson = lessons[b];
+        return lesson;
+    }
+
+}

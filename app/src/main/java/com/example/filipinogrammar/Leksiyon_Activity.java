b4c0e4de2a73eb82515e86ab6904a85c6dc711da
Waquil Carim <CARIM.FAISALWAQUIL@UE.EDU.PH>
2021-05-24 19:52:38 +0800

    Added new Codes

--- app/src/main/java/com/example/filipinogrammar/Leksiyon_Activity.java
+++ app/src/main/java/com/example/filipinogrammar/Leksiyon_Activity.java
@@ -1,6 +1,7 @@
 package com.example.filipinogrammar;
 
 import android.content.Intent;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -20,17 +21,32 @@ public class Leksiyon_Activity extends AppCompatActivity {
         lessonHeader = findViewById(R.id.lesson1);
         lesson = findViewById(R.id.text1);
 
+        Lessons test = new Lessons();
+        String ztest = test.getLessonHeader(0);
+        String xtest = test.getLesson(0);
+
+        lesson.setText((xtest));
+        lessonHeader.setText((ztest));
+
         nextbtn = (Button) findViewById(R.id.btnNext);
         nextbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                //Log.e("hereeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee","lessons");
+                String xtest = test.getLesson(lessons);
+                lesson.setText((xtest));
+
+
                 lessons++;
 
-                lessonHeader.setText(current.getLessonHeader(lessons));
-                lesson.setText(current.getLesson(lessons));
-                if(lessons == 10){
-                    nextbtn.setVisibility(View.INVISIBLE);
+
+                lessonHeader.setText(test.getLessonHeader(lessons));
+                lesson.setText(test.getLesson(lessons));
+                if(lessons == 9){
+                    nextbtn.setVisibility(View.GONE);
                 }
+
             }
         });
 
--- app/src/main/java/com/example/filipinogrammar/Result_Activity.java
+++ app/src/main/java/com/example/filipinogrammar/Result_Activity.java
@@ -9,5 +9,7 @@ public class Result_Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result_);
+
+        
     }
 }

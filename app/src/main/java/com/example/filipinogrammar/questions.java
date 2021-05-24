ff60b6673672ffb2e6a8b81cd034eeaa86fc73f2
njadoon <jadoon.naif@ue.edu.ph>
2021-05-24 14:05:50 +0300

    update

--- app/src/main/java/com/example/filipinogrammar/questions.java
+++ app/src/main/java/com/example/filipinogrammar/questions.java
@@ -1,86 +1,128 @@
 package com.example.filipinogrammar;
-
-import android.content.res.ColorStateList;
+import android.os.CountDownTimer;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Random;
 
 public class questions extends AppCompatActivity {
-    private TextView question, questionNo, timer;
-    private Button btnNext, choice1, choice2, choice3, choice4;
-    private List<QuestionPack> questionList;
-    int totalQuestions;
-    int counter = 0;
+    CountDownTimer timer;
+    int timerValue = 15;
+    private View view;
+    private TextView tvQuestion, tvQuestionNo, quiz, fil;
+    private Button btnNext, btnChoice1, btnChoice2, btnChoice3, btnChoice4, btnStart;
+    private String answer;
+    private int score;
+    private int questionNumber = 1;
     private QuestionPack current;
-    ColorStateList dfRbColor;
-    boolean answer;
+    int n = 0;
+    int questions[] = new int[10];
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_questions);
-
-        questionList = new ArrayList<>();
-        question = findViewById(R.id.question);
-        questionNo = findViewById(R.id.questionNo);
-        timer = findViewById(R.id.timer);
-        choice1 = findViewById(R.id.choice1);
-        choice2 = findViewById(R.id.choice2);
-        choice3 = findViewById(R.id.choice3);
-        choice4 = findViewById(R.id.choice4);
+        tvQuestion = findViewById(R.id.question);
+        fil = findViewById(R.id.fil);
+        quiz = findViewById(R.id.quiz);
+        view = findViewById(R.id.view5);
+        tvQuestionNo = findViewById(R.id.questionNo);
+//        timer = findViewById(R.id.timer);
+        btnChoice1 = findViewById(R.id.choice1);
+        btnChoice2 = findViewById(R.id.choice2);
+        btnChoice3 = findViewById(R.id.choice3);
+        btnChoice4 = findViewById(R.id.choice4);
         btnNext = findViewById(R.id.btnNext);
-
-        dfRbColor = choice1.getTextColors();
-
-        addQuestions();
-        totalQuestions= questionList.size();
-        showNext();
+        btnStart = findViewById(R.id.btnStartQuiz);
+        btnChoice1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btnChoicesOff();
+                if (btnChoice1.getText() == answer) {
+                    score++;
+                }
+            }
+        });
+        btnChoice2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btnChoicesOff();
+                if (btnChoice1.getText() == answer) {
+                    score++;
+                }
+            }
+        });
+        btnChoice3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btnChoicesOff();
+                if (btnChoice1.getText() == answer) {
+                    score++;
+                }
+            }
+        });
+        btnChoice4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btnChoicesOff();
+                if (btnChoice1.getText() == answer) {
+                    score++;
+                }
+            }
+        });
         btnNext.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                showNext();
+                n++;
+                questionNumber++;
+                tvQuestionNo.setText(questionNumber + "/10");
+                updateQuestion(n);
+                btnChoicesOn();
+                btnNext.setEnabled(false);
+            }
+        });
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.e("hereeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee","questionNumber");
+                btnChoice1.setVisibility(View.VISIBLE);
+                btnChoice2.setVisibility(View.VISIBLE);
+                btnChoice3.setVisibility(View.VISIBLE);
+                btnChoice4.setVisibility(View.VISIBLE);
+
+                Random random = new Random();
+                for (int i = 0; i < questions.length; i++) {
+                    questions[i] = random.nextInt(39);
+                    for (int j = 0; j < i; j++) {
+                        if (questions[i] == questions[j]) {
+                            i--;
+                            break;
+                        }
+                    }
+                } updateQuestion(n);
             }
         });
     }
-
-    private void checkAnswer() {
+    private void updateQuestion(int n){
+        tvQuestion.setText(current.getQuestion(questions[n]));
+        btnChoice1.setText(current.getChoice1(questions[n]));
+        btnChoice2.setText(current.getChoice2(questions[n]));
+        btnChoice3.setText(current.getChoice3(questions[n]));
+        btnChoice4.setText(current.getChoice4(questions[n]));
+        answer = current.getCorrectAnswer(questions[n]);
     }
-
-    private void showNext() {
-        if(counter < totalQuestions){
-            choice1.setTextColor(dfRbColor);
-            choice2.setTextColor(dfRbColor);
-            choice3.setTextColor(dfRbColor);
-            current = questionList.get(counter);
-            question.setText(current.getQuestion());
-            choice1.setText(current.getChoice1());
-            choice2.setText(current.getChoice2());
-            choice3.setText(current.getChoice3());
-            choice4.setText(current.getChoice4());
-            counter++;
-            btnNext.setText("Enter");
-            questionNo.setText(counter +"/"+totalQuestions);
-
-        }else {
-            finish();
-        }
+    public void btnChoicesOff(){
+        btnChoice1.setEnabled(false);
+        btnChoice2.setEnabled(false);
+        btnChoice3.setEnabled(false);
+        btnChoice4.setEnabled(false);
     }
-
-    private void addQuestions() {
-        questionList.add(new QuestionPack("Musta?", "OK lang", "Malungkot", "Tama lang", "masaya", 4));
-        questionList.add(new QuestionPack("panget?", "OK lang", "Malungkot", "Tama lang", "masaya", 4));
-        questionList.add(new QuestionPack("pogi?", "OK lang", "Malungkot", "Tama lang", "masaya", 4));
-        questionList.add(new QuestionPack("matangkad?", "OK lang", "Malungkot", "Tama lang", "masaya", 4));
-        questionList.add(new QuestionPack("pandak?", "OK lang", "Malungkot", "Tama lang", "masaya", 4));
-        questionList.add(new QuestionPack("mataba?", "OK lang", "Malungkot", "Tama lang", "masaya", 4));
-        questionList.add(new QuestionPack("Payat?", "OK lang", "Malungkot", "Tama lang", "masaya", 4));
-        questionList.add(new QuestionPack("matalino?", "OK lang", "Malungkot", "Tama lang", "masaya", 4));
-        questionList.add(new QuestionPack("Bobo?", "OK lang", "Malungkot", "Tama lang", "masaya", 4));
-
+    public void btnChoicesOn(){
+        btnChoice1.setEnabled(true);
+        btnChoice2.setEnabled(true);
+        btnChoice3.setEnabled(true);
+        btnChoice4.setEnabled(true);
     }
 }
